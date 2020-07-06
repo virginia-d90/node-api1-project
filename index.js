407973f5d15812688408485b8bee727f1e21d934
Virginia Davenport <virginialdport@gmail.com>
2020-07-06 16:57:06 -0600

    MVP

--- index.js
+++ index.js
@@ -53,6 +53,40 @@ server.get("/api/users/:id", (req, res) => {
     }
 })
 
+server.delete("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    const find = users.find(f => f.id === id);
+    const filterOut = users.filter(f => f.id !== id)
+
+    if(find){
+       users = filterOut
+       res.status(200).json(find) 
+    } else if (users.length === 0){
+        res.status(500).json({errorMessage:"The user could not be removed"})
+    } else {
+        res.status(400).json({errorMessage:"user with specified id can not be found"})
+    }
+})
+
+server.put("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    let found = users.find(h => h.id === id);
+
+
+    if(!found){
+        res.status(404).json({errorMessage: "user with specified ID does not exist"})
+    } else if (!changes.name || !changes.bio){
+        res.status(400).json({errorMessage:"please provide name and bio"})
+    } else if (found){
+        Object.assign(found, changes);
+        res.status(200).json(found)
+    } else {
+        res.status(500).json({errorMessage: "the user information could not be modified"})
+    }
+})
+
 
 const PORT = 5000;
 server.listen(PORT, () => console.log(`server running on port ${PORT}`))
